554f2fa413ed6d65eb9fb3bcc5a22a03dd194226
GreenJeans <40717979+groscoe42@users.noreply.github.com>
2018-10-01 10:45:33 -0400

    init

---
+++ firebase.js
@@ -0,0 +1,16 @@
+var app_firebase = {};
+ (function(){
+ // Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyCzV3uup5hMyfXqML9oOb81r_dLQcUHlNk",
+   authDomain: "feed-dcfe4.firebaseapp.com",
+   databaseURL: "https://feed-dcfe4.firebaseio.com",
+   projectId: "feed-dcfe4",
+   storageBucket: "feed-dcfe4.appspot.com",
+   messagingSenderId: "284436246284"
+ };
+ firebase.initializeApp(config);
+
+ firebase_app = firebase;
+
+})()
