fb652a75b3356cf0f88cb9f2c90bcc4deeb621c7
GreenJeans <40717979+groscoe42@users.noreply.github.com>
2018-10-01 11:01:50 -0400

    done

---
+++ test.js
@@ -0,0 +1,23 @@
+var mainapp = {};
+
+(function(){
+    var firebase = firebase_app;
+    var uid = null;
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      uid = user.uid;
+    }else{
+        //redirect to login page
+        uid = null;
+        window.location.replace("index.html");
+    }
+  });
+
+  function logOut(){
+      firebase.auth().signOut();
+  }
+
+  mainApp.logOut = logOut;
+
+})()
